0b1574ae859107b966acc4c08eb388fc17024255
Chris <chris.gr.skinner@gmail.com>
2022-01-10 16:24:38 +0000

    pre-course revision

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+revision = ["python", "javascript", "other_stuff"]
+boolean = False
+Number = 3
+string = "This is a string"
+null = []
+
+if len(revision) > Number:
+    print(len(revision)*Number)
+else:
+    print(Number+len(revision))
+
+
+
